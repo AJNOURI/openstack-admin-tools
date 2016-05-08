2d792f69ea712c801ecf525bc06dc67e269b63e0
AJ NOURI <ajn.bin@gmail.com>
2016-05-08 08:24:06 +0200

    Update mservices.sh

--- mservices.sh
+++ mservices.sh
@@ -7,12 +7,10 @@ NC='\033[0m' # No Color
 
 
 osservices=(\
- gns3 \
- sshd \
  openstack-nova-api \
  openstack-nova-cert \
  openstack-nova-consoleauth \
- openstack-nova-scheduler \ 
+ openstack-nova-scheduler \
  openstack-nova-conductor \
  openstack-nova-novncproxy \
  neutron-server \
@@ -25,9 +23,6 @@ osservices=(\
  openstack-cinder-scheduler\
  )
 
-#echo "${osservices[@]}"
-
-
 for i in "${osservices[@]}"
 do 
   REZ=$(systemctl is-enabled $i.service 2>&-)
@@ -37,8 +32,6 @@ do
   	printf "$i ==> ${RED}NOK${NC}\n" 
   	#printf "$i ==> \x1b[5m NOK x1b[25m \n"
   fi
-  #echo $REZ
-  
- done
+done
 
  
